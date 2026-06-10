ad94c2925e3f5919fa3ea3b94dfdc5dad6830ac4
Rolf <rolf.schuller@schulen.nuernberg.de>
2026-06-10 17:17:25 +0200

    Spannungteiler Fkt implementiert

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ def strom_berechnen(leistung, spannung):
 # Funktion 2
 def spannungsfall(strom, widerstand):
     # Todo
-    pass
+    # Spannungsfall = Strom * Widerstand
+    return strom * widerstand
 
 # Funktion 3
 def verlustleistung(strom, widerstand):
